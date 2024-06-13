b758e745b425d7911bd63ca737fd37a5a62a30b6
User <User@WINDOWS-FUOISP5>
2024-06-13 13:53:00 +0900

    メルアドチェック用jarの追加

---
+++ src/main/java/form/AdressCheckTest.java
@@ -0,0 +1,22 @@
+package form;
+
+import org.apache.commons.validator.routines.EmailValidator;
+
+public class AdressCheckTest {
+	public static void main(String[] args) {
+		
+		String value = "test//@gmail.com";
+		
+		boolean result = false;
+		
+	    if (value != null) {
+	        if(EmailValidator.getInstance().isValid(value)) {
+	            result = true;
+	        }
+	    }
+	   System.out.println(result);
+		
+	}
+	
+	
+}
--- src/main/webapp/Accounts0030_Registration.jsp
+++ src/main/webapp/Accounts0030_Registration.jsp
@@ -18,7 +18,7 @@
 	<div class="container-fluid">
 		<div class="offset-1 col-10" style="margin-top: 10vh;">
 			<h1>アカウント登録</h1>
-			<form class="was-validated" action="">
+			<form class="was-validated" action="Accounts0030" method="post">
 
 				<div class="row g-3 align-items-center">
 					<div class="col-4 d-flex flex-row-reverse">
