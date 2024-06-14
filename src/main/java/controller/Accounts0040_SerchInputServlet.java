d5f575deb1bedd19c438384ce4d56709de232e66
User <User@WINDOWS-JLJC6HT>
2024-06-14 09:36:29 +0900

    アカウント検索結果表示

--- src/main/java/controller/Accounts0040_SerchInputServlet.java
+++ src/main/java/controller/Accounts0040_SerchInputServlet.java
@@ -36,7 +36,7 @@ public class Accounts0040_SerchInputServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.sendRedirect("Accounts0041_SerchResultServlet");
+		response.sendRedirect("Accounts0041_SerchResult");
 	}
 
 }
