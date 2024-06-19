a44d4c6d367bb14a92f87ca5cb3e97a04dd13d1f
User <User@WINDOWS-JLJC6HT>
2024-06-19 13:12:46 +0900

    filter

---
+++ src/main/java/filter/A0031_filter.java
@@ -0,0 +1,71 @@
+package filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.beans.AccountsBean;
+
+/**
+ * Servlet Filter implementation class A0031_filter
+ */
+@WebFilter("/*")
+public class A0031_filter extends HttpFilter implements Filter {
+       
+    /**
+     * @see HttpFilter#HttpFilter()
+     */
+    public A0031_filter() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+			var req = (HttpServletRequest) request;
+			var res = (HttpServletResponse) response;
+
+			String path = req.getServletPath();
+			
+			HttpSession session = req.getSession();
+			
+			AccountsBean ab = (AccountsBean)session.getAttribute("accounts");
+			
+			if (path.equals("/Accounts0031")){
+				if(ab==null) {
+					res.sendRedirect("Accounts0030");
+					return;
+				}
+			}
+				chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
