27f0012c5a70b3c7b8cc4a0278c844e00ca50920
User <User@WINDOWS-FUOISP5>
2024-06-27 17:05:32 +0900

    jsp用フィルターが保存されていなかった。

---
+++ src/main/java/filter/Jsp.java
@@ -0,0 +1,63 @@
+package filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet Filter implementation class Jsp
+ */
+@WebFilter("/*")
+public class Jsp extends HttpFilter implements Filter {
+       
+    /**
+     * @see HttpFilter#HttpFilter()
+     */
+    public Jsp() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+		HttpServletRequest httpRequest = (HttpServletRequest)request;
+	    HttpServletResponse httpResponse = (HttpServletResponse)response;
+
+	    String path= httpRequest.getServletPath();
+
+		if(path.contains(".jsp")) {
+			httpResponse.sendRedirect("C0020");
+			return;
+		}
+		
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
